d6cf8e0e1b4e44d232d27750a39b969199bfb2b9
Maxence Delaine <maxence.delaine@gmail.com>
2018-03-20 18:33:52 +0100

    Modifs ***not working***

--- js/responsify_custom.js
+++ js/responsify_custom.js
@@ -1,6 +1,6 @@
-$(window).load(function(){
-  $('img').responsify({});
-});
+$(window).load(function() {
+   $('img').responsify();
+ });
 $(window).resize(function(){
-  $('img').responsify({});
-});
+    $('img').responsify();
+  });
